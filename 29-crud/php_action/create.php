f2dfe15956ce7508b9bcd593a182cdcc9773c36b
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-10-17 15:00:45 -0300

    XSS (Cross Site Scripting) como se proteger

--- 29-crud/php_action/create.php
+++ 29-crud/php_action/create.php
@@ -3,12 +3,26 @@
     session_start();
 // Conexão
     require_once 'db_connect.php';
+// Clear XSS (Cross Site Scripting)
+/*
+    Ataque Cross Site Scripting
+    semelhante ao SQL Injection porém ao invés 
+    de inserir comandos SQL o invasor insere scripts no sistema.
+*/
+function clear($input) {
+    global $connect;
+    // Proteção contra SQL Injection
+    $var = mysqli_escape_string($connect, $input);
+    // Proteção contra XSS
+    $var = htmlspecialchars($var);
+    return $var;
+}
 
     if (isset($_POST['btn-cadastrar'])) {
-        $nome = mysqli_escape_string($connect, $_POST['nome']);
-        $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-        $email = mysqli_escape_string($connect, $_POST['email']);
-        $idade = mysqli_escape_string($connect, $_POST['idade']);
+        $nome = clear($_POST['nome']);
+        $sobrenome = clear($_POST['sobrenome']);
+        $email = clear($_POST['email']);
+        $idade = clear($_POST['idade']);
         $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')";
 
         if (mysqli_query($connect, $sql)) {

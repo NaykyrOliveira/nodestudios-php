617d5e416f9180f214826db19da237c149cde3d4
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-10-27 09:05:01 -0300

    Tratamento de exeções

---
+++ poo-php/tratamento-de-execoes.php
@@ -0,0 +1,25 @@
+<?php
+// Ocorrência anormal que afeta o funcionamento da aplicação
+// Exception é a classe base de todas as Exceptions
+// message, code, file, line
+
+class Newsletter {
+    public function cadastrarEmail($email) {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("Este email é inválido" ,1);
+        } else {
+            echo "Email cadastrado com sucesso!";
+        }
+    }
+}
+
+$newsletter = new Newsletter();
+
+try {
+    $newsletter -> cadastrarEmail("contato@");
+} catch(Exception $e) {
+    echo "Mensagem: ".$e -> getMessage()."<br>"; 
+    echo "Código: ".$e -> getCode()."<br>";
+    echo "Linha: ".$e -> getLine()."<br>";
+    echo "Arquivo: ".$e ->getFile();
+}
